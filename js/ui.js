c3c9363acda0d5e2e18ad5408cbe7114ca07c433
sbolognese <sabrinalbolognese@gmail.com>
2018-10-21 21:10:29 -0700

    preliminary commands added

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,38 @@
-"use strinct";
-nAnimationTime = 800;
+"use strict";
+var nAnimationTime = 800;
+
+var typicalUserGreetings = ["hello", "hi", "hey", "sup"];
 
 $(document).ready(function(){
 	$('#fun').click(function(){
 		$('#console').slideToggle(nAnimationTime);
 	});
+
+
+	var input = document.getElementById("userInput");
+	input.addEventListener("keyup",function(event){
+		event.preventDefault();
+		if(event.keyCode===13){
+			handleInput($('#userInput').val());
+		}
 });
+});
+
+
+function handleInput(sText){
+	sText = sText.replace(/\?|\.|[0..9]|\!/g,'');
+	writeToFakeConsole(sText, ">");
+	$('#userInput').val("");
+	if(typicalUserGreetings.includes(sText)){
+		sText = "hello"
+	}
+	if(acceptableCommands.hasOwnProperty(sText)){
+		acceptableCommands[sText]();
+	} else {
+		unknownCommand();
+	}
+}
+
+function writeToFakeConsole(sText, sPrefix = ""){
+	$('#log').append('<br>' + sPrefix + sText).scrollTop(1E10);;
+}
